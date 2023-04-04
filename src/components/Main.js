9ba1a79d1229076f70ac1a1d36645a1a0cbdda24
sk131453 <sk131453@gmail.com>
2023-04-04 10:19:15 +0530

    feat: basic setup

---
+++ src/components/Main.js
@@ -0,0 +1,16 @@
+// import Header from './Header' ;
+import Home from './Home';
+import Header from './Header';
+import Footer from './Footer';
+
+function Main() {
+    return (
+      <div>
+        <Header />
+         <Home/>
+         <Footer/>
+      </div>
+    );
+  }
+  
+  export default Main;
