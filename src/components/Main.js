add1f3d621c6523ff8fc4be45b62278130c77682
sk131453 <sk131453@gmail.com>
2023-04-05 10:27:06 +0530

    commited main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,23 @@
 // import Header from './Header' ;
 import Home from './Home';
+import CourseDetail from "./CourseDetail";
 import Header from './Header';
+import About from './About';
 import Footer from './Footer';
+
+import { Routes as Switch,Route } from 'react-router-dom';
 
 function Main() {
     return (
       <div>
         <Header />
-         <Home/>
-         <Footer/>
+        <Switch>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/about" element={<About/>}/>
+          <Route path="/detail/:course_id" element={<CourseDetail />}/>
+        </Switch>
+         
+        <Footer/>
       </div>
     );
   }
