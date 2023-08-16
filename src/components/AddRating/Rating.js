5aaf3f8359d32f6a7f0b86ef7bf5a18f69343e75
sk131453 <sk131453@gmail.com>
2023-08-16 12:05:37 +0530

    Rating And Rate Pages Completed

---
+++ src/components/AddRating/Rating.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import Rate from "./Rate";
+
+const Rating = () => {
+  const [rating, setRating] = useState(0);
+  
+  return (
+    <>
+      <div className="row">
+        <div className="col text-center">
+          <Rate rating={rating} onRating={(rate) => setRating(rate)} />
+          <p>Rating - {rating}</p>
+        </div>
+      </div>
+    </>
+  );
+};
+//color={{filled: "rgb(136 87 25)", unfilled: "rgb(214 184 147)"}}
+//count={10}
+export default Rating;
